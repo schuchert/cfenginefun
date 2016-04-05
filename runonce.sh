356309f68dd79498e6b82d7990abead94c84e07b
Brett Schuchert <brett@industriallogic.com>
2016-04-05 14:33:08 -0500

    [Chris, Brett] verified on Windows git bash and os x shell.

--- runonce.sh
+++ runonce.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-NAME=ubuntu_1504.box
+NAME=ubuntu_1504
+BOX=$NAME.box
 
-if [ ! -f "$NAME" ]; then
-  curl -L https://github.com/kraksoft/vagrant-box-ubuntu/releases/download/15.04/ubuntu-15.04-amd64.box --output $NAME
-  vagrant box add $NAME ubuntu_1504
+if [ ! -f "$BOX" ]; then
+	  curl -L https://github.com/kraksoft/vagrant-box-ubuntu/releases/download/15.04/ubuntu-15.04-amd64.box --output $BOX
 fi
+
+vagrant box list 2>/dev/null | grep -q "\b${NAME}\b" || { 
+  vagrant box add $NAME $BOX
+}
 
 vagrant up --provision
 vagrant halt
 vagrant up
-
